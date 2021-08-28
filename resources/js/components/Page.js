760a086b3c8b437a1534447c3667ae7c3355fb90
Adrian Lopez Iglesias <adrianlopeziglesias1@gmail.com>
2021-08-28 14:29:41 -0300

    Comenzando routing

--- resources/js/components/Page.js
+++ resources/js/components/Page.js
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 //components
 import About from './About';
@@ -11,38 +11,60 @@ import Contact from './Contact';
 /* react-intl imports */
 import { IntlProvider } from 'react-intl';
 import LanguageWrapper from './LanguageWrapper'
-import {Context} from './LanguageWrapper'
+import { Context } from './LanguageWrapper'
 
 
 import Container from 'react-bootstrap/Container'
+
+//ROUTER
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function Page() {
   const context = useContext(Context);
 
   return (
+    <Router>
       <div>
-      <LanguageWrapper>
-        <Navigation context={context} />
-        <br />
-        <Container>
-<br />
-<br />
-        <About />
-<br />
-<br />
-        <Skills />
-<br />
-<br />
-          <Projects />
-<br />
-<br />
-        {/* <Stack /> */}
-        <Contact />
+        <LanguageWrapper>
+          <Navigation context={context} />
           <br />
-          <br />
-        </Container>
-    </LanguageWrapper>
+          <Container>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+
+
+
+            <br />
+            <br />
+            <Skills />
+            <br />
+            <br />
+            <Projects />
+            <br />
+            <br />
+            {/* <Stack /> */}
+            <Contact />
+            <br />
+            <br />
+          </Container>
+        </LanguageWrapper>
       </div>
+    </Router>
   );
 }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,34 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Adrian Lopez Iglesias - Desarrollos IT</title>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-PWTQF87');
+  </script>
+  <!-- End Google Tag Manager -->
 </head>
-@section('content')
 
-<div class="" id="App">
+<body>
+  @section('content')
 
-</div>
+  <div class="" id="App">
+
+  </div>
+</body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWTQF87" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <script src="{{ mix('js/app.js') }}"></script>
