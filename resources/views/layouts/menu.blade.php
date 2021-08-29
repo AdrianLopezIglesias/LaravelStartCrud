7766c6e1aacb585ac1ee4b38c9b1d41d18554acb
Adrian Lopez Iglesias <adrianlopeziglesias1@gmail.com>
2021-08-28 22:08:24 -0300

    Se limpio infyom

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,25 +1,4 @@
-<li class="nav-item">
-    <a href="{{ route('teclados.index') }}"
-       class="nav-link {{ Request::is('teclados*') ? 'active' : '' }}">
-        <p>Teclados</p>
-    </a>
-</li>
 
-
-<li class="nav-item">
-    <a href="{{ route('mice.index') }}"
-       class="nav-link {{ Request::is('mice*') ? 'active' : '' }}">
-        <p>Mice</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('projects.index') }}"
-       class="nav-link {{ Request::is('projects*') ? 'active' : '' }}">
-        <p>Projects</p>
-    </a>
-</li>
 
 
 <li class="nav-item">
